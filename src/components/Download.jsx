4d67b324aeadeab53a674006a2ec3efc7ceba34d
Chau Trinh <trinhchau.1006@gmail.com>
2022-09-28 15:21:30 +0700

    first commit

---
+++ src/components/Download.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "../styles/Global";
+import assets from "../assets";
+
+const Download = () => {
+  return (
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>
+            Download the Source Code
+          </h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>
+            Get the full source code on GitHub
+          </p>
+        </div>
+        <a
+          className={styles.btnPrimary}
+          href='https://www.google.com'
+          type="button"
+          target="_blank"
+        >
+          Source Code
+        </a>
+        <div className={styles.flexCenter}>
+          <img src={assets.scene} alt="Download" className={styles.fullImg} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Download;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Button from "./Button";
+import Download from "./Download";
+import Features from "./Features";
+import SectionWrapper from "./SectionWrapper";
+
+export { Features, Download, SectionWrapper, Button };
